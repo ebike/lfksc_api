bcf3e427745476c68ca83d6514e6e082be3da07c
jimmy <hu.gw@ikcrm.com>
2016-05-03 22:09:30 +0800

    添加登录接口

--- src/main/java/com/lfksc/api/service/IUserService.java
+++ src/main/java/com/lfksc/api/service/IUserService.java
@@ -2,7 +2,22 @@ package com.lfksc.api.service;
 
 import com.lfksc.api.model.User;
 
-public interface IUserService
-{
-	public void saveUser(User user);
+public interface IUserService {
+
+	/**
+	 * 登录
+	 * 
+	 * @param loginName
+	 *            账号
+	 * @param password
+	 *            密码
+	 * @param clientId
+	 *            设备唯一编号
+	 * @param platform
+	 *            平台（ios/android）
+	 * @return token
+	 */
+	public String login(String loginName, String password, String clientId, String platform);
+
+
 }
